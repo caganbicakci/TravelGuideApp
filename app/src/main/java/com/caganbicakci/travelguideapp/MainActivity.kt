07f78d2c03d7d96ce9e66213fd13ab1e5e94c9da
Çağan Bıçakçı <caganbicakci97@gmail.com>
2022-09-28 21:33:17 +0300

    bottom navigation added

--- app/src/main/java/com/caganbicakci/travelguideapp/MainActivity.kt
+++ app/src/main/java/com/caganbicakci/travelguideapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.caganbicakci.travelguideapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.caganbicakci.travelguideapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navHostFragment : NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            navHostFragment = supportFragmentManager.findFragmentById(fragmentContainerView.id) as NavHostFragment
+            NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+        }
+
     }
 }
